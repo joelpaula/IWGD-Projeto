bfe60bfafc2dd8e8c40dff0f2baa27b00707fc68
joelpaula <joelpaula@hotmail.com>
2021-10-19 23:01:46 +0100

    Model and Migrations done

--- music_website/collection/models.py
+++ music_website/collection/models.py
@@ -34,7 +34,7 @@ class Record(models.Model):
     artist_id = models.ForeignKey(Artist, on_delete=models.CASCADE) #artist_id FK
     discogs_release_id = models.IntegerField() # album id as in discogs
     title = models.CharField(max_length=200) # album title
-    year = models.IntegerField(max_length=4) # album release date
+    year = models.IntegerField() # album release date
     cover_url = models.URLField() # album front cover url
 
     def __str__(self):
@@ -43,18 +43,25 @@ class Record(models.Model):
 
 class Collection_Record(models.Model):
     """links unique record to a user collection | id; record_id(FK PK); collection_id(FK PK); tag"""
-    record_id = models.ForeignKey(Record, on_delete=models.CASCADE, primary_key=True) # PK FK
-    collection_id = models.ForeignKey(Collection, primary_key=True) # PK FK
+    record_id = models.ForeignKey(Record, on_delete=models.CASCADE) # FK
+    collection_id = models.ForeignKey(Collection, on_delete=models.CASCADE) # FK
     tag = models.CharField(max_length=100) #tag field, editable by user
 
     def __str__(self):
         return f"Album {Record.objects.get(pk=self.record_id).title} na coleção {Collection.objects.get(pk=self.collection_id).name} do user {Collection.objects.get(pk=self.collection_id).user_id}" # TODO: tentar aceder a atributos de classes do models assim funciona?
+    class Meta:
+        managed = False
+        db_table = 'Collection_Record'
+        unique_together = (('record_id', 'collection_id'),)
 
 
 class Rating(models.Model):
     """links a user to a rating/review of a specif record | id; user_id(FK PK); discogs_release_id(FK PK); rating; review"""
-    user_id = user_id = models.ForeignKey(User, on_delete=models.CASCADE, primary_key=True) # user_id FK PK
-    record_id = models.IntegerField(primary_key=True) # record_id FK PK
-    rating = models.IntegerField(MinValueValidator(0, message="Rating mínimo é 0"), MaxValueValidator(5, message="Rating máximo é 5"))
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE) # user_id FK PK
+    record_id = models.ForeignKey(Record, on_delete=models.CASCADE) # PK FK
+    rating = models.IntegerField(validators=[MinValueValidator(0, message="Rating mínimo é 0"), MaxValueValidator(5, message="Rating máximo é 5")])
     review = models.TextField() # review
-
+    class Meta:
+        managed = False
+        db_table = 'Rating'
+        unique_together = (('user_id', 'record_id'),)
--- music_website/home/views.py
+++ music_website/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def home_index(request):
-    pass
+    HttpResponse("Welcome!")
