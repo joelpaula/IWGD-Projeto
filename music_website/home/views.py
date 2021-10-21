818c1e4435baa1b65360feed6556197aa23853b5
João Martins <joao.dm.martins@gmail.com>
2021-10-21 17:18:33 +0100

    view artist

--- music_website/home/views.py
+++ music_website/home/views.py
@@ -3,8 +3,6 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from music_website.collection.models import Like_Artist
-import requests
-from django.http import JsonResponse
 import discogs_client
 
 
@@ -16,8 +14,15 @@ def search(request, collection_id = None):
     pass
 
 
-def artist(request, collection_id, artist_id):
-    collection_to_add_to = None
+def save_like_artist(request, artist_id, like):
+    if like == False:
+        #get POST
+    
+    pass
+
+
+def artist(request, artist_id, collection_id=None):
+    collection_to_add_to = collection_id
     like = False
     if request.user.is_authenticated:
         if collection_id is not None:
@@ -26,15 +31,28 @@ def artist(request, collection_id, artist_id):
     
     # TODO: montar acesso/autenticação discogs
 
-    discogs_artist = d.artist(artist_id) # d = acesso discogs
-    # mostra nome de artista
-    artist_name = discogs_artist.name  #TODO: verificar atributo
-    # mostra bio de artista
-    artist_bio = discogs_artist.profile # TODO: verfificar atributo
-    # mostra foto do artista
-    artist_pic = discogs_artist.images # TODO: verfificar atributo
-    # mostra lista de albums do artista
-    artist_releases = discogs_artist.releases # TODO: verfificar atributo
-    context = {'artist_name': artist_name, 'artist_bio': artist_bio, 'artist_pic': artist_pic, 'artist_releases': artist_releases}
+    discogs_artist = d_session.artist(artist_id) # d_session = acesso através module python discogs
+    artist_name = discogs_artist.name  # para mostrar nome de artista #TODO: verificar atributo
+    artist_bio = discogs_artist.profile # para mostrar bio de artista # TODO: verfificar atributo
+    artist_pic = discogs_artist.images # para mostrar foto do artista # TODO: verfificar atributo
+    artist_releases = discogs_artist.releases # para mostrar lista de albums do artista # TODO: verfificar atributo
+    
+    context = {'collection_to_add_to': collection_to_add_to, 'like': like, 'artist_name': artist_name, 'artist_bio': artist_bio, 'artist_pic': artist_pic, 'artist_releases': artist_releases}
     template = 'home/artist.html'
+    
     return render(request, template, context)
+
+
+
+
+def record(request, record_id):
+    # cover
+    # rating
+    # nbr of votes
+    # collection name
+    # nbr faixa, nome_faixa, duracao_faixa
+    pass
+
+
+def play(request):
+    pass
