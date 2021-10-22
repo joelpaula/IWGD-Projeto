8fd6da345ce0ef84956a30d79e7a3756d2975b88
João Martins <joao.dm.martins@gmail.com>
2021-10-22 16:11:12 +0100

    artist view

--- music_website/home/views.py
+++ music_website/home/views.py
@@ -3,15 +3,16 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from models import Like_Artist
-import discogs_client
+from discogs import discog_artist, discog_record
 
 
 def home_index(request):
     HttpResponse("Welcome!")
 
 
-def search(request, collection_id = None):
+def search_artist(request, collection_id = None):
     pass
+    #return d_artist
 
 
 def save_like_artist(request, artist_id, like):
@@ -24,23 +25,24 @@ def get_artist(request, artist_id):
     pass
 
 
-def artist(request, artist_id, collection_id=None):
+def artist(request, d_artist, collection_id=None):
+    """IN: d_artist (class discog_artist), collection_id (int) |
+    processa dados para página do Artista;
+    se user autenticado, busca info do like;
+    se user autenticado e há collection_id != None, define-se o campo collection_to_add_to"""
     collection_to_add_to = collection_id
     like = False
     if request.user.is_authenticated:
         if collection_id is not None:
             collection_to_add_to = collection_id
         like = Like_Artist.objects.get(user_id=request.user.id).like #TODO: verificar atributo
-    
-    # TODO: montar acesso/autenticação discogs
 
-    discogs_artist = d_session.artist(artist_id) # d_session = acesso através module python discogs
-    artist_name = discogs_artist.name  # para mostrar nome de artista #TODO: verificar atributo
-    artist_bio = discogs_artist.profile # para mostrar bio de artista # TODO: verfificar atributo
-    artist_pic = discogs_artist.images # para mostrar foto do artista # TODO: verfificar atributo
-    artist_releases = discogs_artist.releases # para mostrar lista de albums do artista # TODO: verfificar atributo
+    artist_name = d_artist.title  # para mostrar nome de artista
+    artist_bio = d_artist.bio # para mostrar bio de artista 
+    artist_pic = d_artist.cover_image # para mostrar foto do artista 
+    # artist_releases = discogs_artist.releases # para mostrar lista de albums do artista # TODO: artist_releases
     
-    context = {'collection_to_add_to': collection_to_add_to, 'like': like, 'artist_name': artist_name, 'artist_bio': artist_bio, 'artist_pic': artist_pic, 'artist_releases': artist_releases}
+    context = {'collection_to_add_to': collection_to_add_to, 'like': like, 'artist_name': artist_name, 'artist_bio': artist_bio, 'artist_pic': artist_pic, 'artist_releases': artist_releases, 'd_artist_id': d_artist.discogs_id}
     template = 'home/artist.html'
     
     return render(request, template, context)
