3aee2d6e5fe0d913c1538a45d193243b18b0dc88
joelpaula <joelpaula@hotmail.com>
2021-10-22 19:43:48 +0100

    Base+Home structure

--- music_website/music_website/urls.py
+++ music_website/music_website/urls.py
@@ -18,7 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path(''), include('home.urls'),
-    path('home/', include('home.urls')),
+    path('', include('home.urls')),
     #path('collection/', include('collection.urls'))
 ]
